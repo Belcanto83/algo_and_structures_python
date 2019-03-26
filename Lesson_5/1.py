1f8af11e8a35ef20bf0c53441ad8e520eac87025
Aleksey <farego@yandex.ru>
2019-03-26 22:11:09 +0300

    Lesson 5

--- Lesson_5/1.py
+++ Lesson_5/1.py
@@ -5,3 +5,46 @@
 вывести наименования предприятий, чья прибыль выше среднего и отдельно
 вывести наименования предприятий, чья прибыль ниже среднего.
 """
+
+from collections import namedtuple
+
+n = int(input('Введите количество предприятий: '))
+
+organizations_dict = {}
+
+for i in range(n):
+    organization_name = input(f'Введите название {i+1}-й огранизации: ')
+    organization_profit = tuple(
+        float(elem) for elem in input('Введите прибыль организации за 4 квартала (4 числа через пробел): ').split()
+    )
+    organizations_dict[organization_name] = organization_profit
+
+Organizations = namedtuple('Organizations', organizations_dict)
+organizations = Organizations(**organizations_dict)
+print()
+print('*'*100)
+print(organizations)
+print('*'*100)
+
+profit_list = []
+common_profit = 0
+for organization in organizations:
+    profit = sum(organization)
+    common_profit += profit
+    profit_list.append(profit)
+average_profit = round(common_profit / len(organizations), 2)
+
+s_high_profitable_orgs = f'Организации с годовой прибылью, выше или равной средней {average_profit}:\n'
+s_low_profitable_orgs = f'Организации с годовой прибылью, ниже средней {average_profit}:\n'
+
+i = 0
+for organization in organizations:
+    name = organizations._fields[i]
+    if profit_list[i] >= average_profit:
+        s_high_profitable_orgs += name + ' - ' + str(profit_list[i]) + '\n'
+    else:
+        s_low_profitable_orgs += name + ' - ' + str(profit_list[i]) + '\n'
+    i += 1
+
+print()
+print(s_high_profitable_orgs, s_low_profitable_orgs, sep='\n')
--- Lesson_5/2.py
+++ Lesson_5/2.py
@@ -5,3 +5,62 @@
 [‘C’, ‘4’, ‘F’] соответственно. Сумма чисел из примера: [‘C’, ‘F’, ‘1’],
 произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
 """
+
+hex_num_1 = list(input('Пожалуйста, введите первое шестнадцатеричное число: '))
+hex_num_2 = list(input('Пожалуйста, введите второе шестнадцатеричное число: '))
+print(hex_num_1, hex_num_2)
+
+
+def hex_sum(num1, num2):
+    from collections import deque
+
+    n1 = deque(num1)
+    n2 = deque(num2)
+
+    hex_order = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f')
+
+    i = 0
+    n1.reverse()
+    number_1 = n1[i]
+
+    n2.reverse()
+    number_2 = n2[i]
+
+    result = deque()
+    remember_category = False
+    while number_1 in n1 or number_2 in n2 or remember_category:
+        index = (hex_order.index(number_1) + hex_order.index(number_2)) % 16
+        new_hex_number = hex_order[index]
+
+        if index < 15 and remember_category:
+            new_hex_number = hex_order[index + 1]
+            remember_category = False
+        elif index == 15 and remember_category:
+            new_hex_number = hex_order[0]
+            remember_category = True
+        else:
+            remember_category = False
+
+        if (hex_order.index(number_1) + hex_order.index(number_2)) // 16:
+            remember_category = True
+
+        result.appendleft(new_hex_number)
+
+        i += 1
+        if i < len(n1):
+            number_1 = n1[i]
+            number_1 = number_1.lower()
+        else:
+            number_1 = '0'
+        if i < len(n2):
+            number_2 = n2[i]
+            number_2 = number_2.lower()
+        else:
+            number_2 = '0'
+
+    result = list(result)
+
+    return result
+
+
+print('Результат сложения: ', hex_sum(hex_num_1, hex_num_2))
